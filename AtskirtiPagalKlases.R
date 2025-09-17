f3aea4b5594e1eb80286fdfe29a8c613e74c3827
Liveta <livetakaval@gmail.com>
2025-09-17 23:11:55 +0300

    Duomenų rinkinys išskirtas į tris skirtingus failus, kad būtų lengviau skaičiuoti išskirtis

---
+++ AtskirtiPagalKlases.R
@@ -0,0 +1,9 @@
+﻿data <- read.csv('csv/duomenys_be_praleistu_reiksmiu.csv', sep = ',')
+
+data_0 <- data[data$label == 0, ]
+data_1 <- data[data$label == 1, ]
+data_2 <- data[data$label == 2, ]
+
+write.csv(data_0, 'csv/duomenys_be_praleistu_reiksmiu/label_0.csv', row.names = FALSE)
+write.csv(data_1, 'csv/duomenys_be_praleistu_reiksmiu/label_1.csv', row.names = FALSE)
+write.csv(data_2, 'csv/duomenys_be_praleistu_reiksmiu/label_2.csv', row.names = FALSE)
--- PraleistosReiksmes.R
+++ PraleistosReiksmes.R
@@ -1,7 +1,7 @@
 ﻿raw_data<- read.csv("csv/atrinkti_duomenys_6_rodikliai.csv", sep = ",")
 
 # praleistų reikšmių nustatymas
-pr_reiksmes <- mapply(anyNA, raw_data)
+pr_reiksmes <- colSums(is.na(raw_data))
 
 # praleistų reikšmių šalinimas
 data <- raw_data[!is.na(raw_data$R_val), ]
