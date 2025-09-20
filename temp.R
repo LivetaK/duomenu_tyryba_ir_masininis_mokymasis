ed47903c7a87d392fbb4fecf3abe66e0dea262d4
nešiojamas <Admin@DESKTOP-0LCIBFD>
2025-09-20 13:48:54 +0300

    Prideti failai tyrimui

---
+++ temp.R
@@ -0,0 +1,14 @@
+﻿df <- read.csv("csv/atrinkti_duomenys.csv", sep = ",", header = TRUE, check.names = FALSE) # Nuskaitome failą
+
+# --- Aprašomoji duomenų analizė (bendra)
+
+cols_to_keep <- c("signal_std",
+                  "wl_side",
+                  "R_val",
+                  "RR_r_0.RR_r_1",
+                  "P_val",
+                  "Q_val",
+                  "label") # Parenkame kuriuos rodiklius norime pasilikti analizei
+
+df_selected <- df[, cols_to_keep] # Sukuriame naują duomenų rinkinį su 7 pasirinktais rodikliais
+write.csv(df_selected, "csv/atrinkti_duomenys_6_rodikliai_0918.csv", row.names = FALSE) # Išsaugome naują duomenų rinkinį į CSV failą
