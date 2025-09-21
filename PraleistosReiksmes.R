b91d1783f3dfe1237ba6e848808a372f14aa2d1e
LivetaK <159531709+LivetaK@users.noreply.github.com>
2025-09-21 14:09:25 +0300

    Duomenų rinkinys išskirtas į tris skirtingus failus, kad būtų lengvia… (#5)
    * Duomenų rinkinys išskirtas į tris skirtingus failus, kad būtų lengviau skaičiuoti išskirtis
    
    * Prideti failai tyrimui
    
    * Prideti failai tyrimui
    
    * Pridėti komentarai, pakoreguotas kodas, kad būtų galima nusistatyti kokių išskirčių ieškome. Nuimtas stulpelis "is_outlier" iš csv failų
    
    ---------
    
    Co-authored-by: nešiojamas <Admin@DESKTOP-0LCIBFD>

--- PraleistosReiksmes.R
+++
@@ -1,9 +0,0 @@
-﻿raw_data<- read.csv("csv/atrinkti_duomenys_6_rodikliai.csv", sep = ",")
-
-# praleistų reikšmių nustatymas
-pr_reiksmes <- mapply(anyNA, raw_data)
-
-# praleistų reikšmių šalinimas
-data <- raw_data[!is.na(raw_data$R_val), ]
-
-write.csv(data, "csv/duomenys_be_praleistu_reiksmiu.csv", row.names = FALSE)
